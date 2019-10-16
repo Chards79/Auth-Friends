c23868572042bb85ba1e0b313ed787929a0e4af3
Eric Richards <esholundr@yahoo.com>
2019-10-15 21:17:28 -0600

    Edit, Delete, and Cancel buttons with functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './components/Login';
 import FriendList from './components/FriendList';
-import FriendForm from './components/FriendForm';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <Route path="/login" component={Login} />
-      <PrivateRoute path="/" component={FriendList} />
-      <Route path="/form" component={FriendForm} />
+      <PrivateRoute exact path="/" component={FriendList} />
     </Router>
   );
 }
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
+import FriendForm from './FriendForm';
 
 export default function FriendList() {
     const [friends, setFriends] = useState([]);
+    const [editingFriend, setEditingFriend] = React.useState();
+
     const fetchFriends = () => {
         axiosWithAuth()
             .get("/api/friends")
@@ -12,16 +15,45 @@ export default function FriendList() {
             })
             .catch(err => console.log(err.response));
     };
+
     React.useEffect(() => {
         fetchFriends()
-    }, [])
+    }, []);
+
+    const deleteFriend = () => {
+        axiosWithAuth()
+            .delete(`/api/friends/${id}`)
+            .then(res => {
+                console.log(res);
+                setFriends(res.data);
+            }).catch(err => console.log(err.response));
+    };
+
+    const editFriend = friendObj => {
+        setEditingFriend(friendObj);
+    }
 
     return (
         <div>
-            <button onClick={fetchFriends}>Update!</button>
+            <FriendForm
+                editingFriend={editingFriend}
+                setFriends={setFriends}
+                setEditingFriend={setEditingFriend}
+            />
             {friends.map(friendObj => {
-                return <div key={friendObj.id}> {friendObj.name} </div>;
+                return (
+                    <div key={friendObj.id}>
+                        {" "}
+                        <p>Name: {friendObj.name}</p>
+                        <p>Age: {friendObj.age}</p>
+                        <p>Email: {friendObj.email}</p>
+                        <button onClick={() => editFriend(friendObj)} >EDIT</button>{" "}
+                        <button onClick={() => deleteFriend(friendObj.id)}>DELETE</button>{" "}
+
+                    </div>
+                );
             })}
+            <button onClick={fetchFriends}>Update!</button>
         </div>
     );
 }
